4503b4390a4e078568adcd9cd8f1ffc70355eaa1
AmiraliE1380 <60638047+AmiraliE1380@users.noreply.github.com>
2023-05-31 17:14:20 +0330

    Add files via upload

--- MC model/MC_selfish_mining_chia_ver2_d=8.pm
+++ MC model/MC_selfish_mining_chia_ver2_d=8.pm
@@ -4,14 +4,14 @@ dtmc
 const double x = 0.25;
 //y or gamma is the probability of honest mining on adv blocks during a race condition
 const double y = 0.5;
-// d is the max lead the adv can have
-const int d = 7;
+// d+1 is the max lead the adv can have
+const int d = 8;
 // at each depth 4 private blocks can be saved.
 const int w = 1;
 
 
 // n is total number of private adv. blocks	
-formula n = (n0+n1+n2+n3+n4+n5+n6);
+formula n = (n0+n1+n2+n3+n4+n5+n6+n7);
 formula total = (n*x+1);
 
 module SelfishMiningChia
@@ -24,6 +24,7 @@ module SelfishMiningChia
 	n4 : [0..w] init 0;
 	n5 : [0..w] init 0;
 	n6 : [0..w] init 0;
+	n7 : [0..w] init 0;
 	
 	// h is the lead of the adv
 	h : [0..d] init 0;
@@ -45,12 +46,12 @@ module SelfishMiningChia
 			(1-x)/total : (c'=n0) & (n0'=0) & (h'=0) & (adv'=0) & (honest'=0) ; 
 	
 
-	[] c>0 & h=0 -> (x*c)/(x*c+1-x) : (c'=0) & (adv'=2) +
+	[] c>0 & h=0-> (x*c)/(x*c+1-x) : (c'=0) & (adv'=2) +
 			(1-x)*y/(x*c+1-x) : (c'=0) & (adv'=1) & (honest'=1) +
 			(1-x)*(1-y)/(x*c+1-x) : (c'=0) & (honest'=2); 
 	
 
-	[] h=2 & c=0 -> (1-x)/total : (n0'=0) & (n1'=0) & (n2'=0) & (n3'=0) & (n4'=0) & (n5'=0) & (n6'=0) & (h'=0) & (adv'=2) & (honest'=0) +
+	[] h=2 & c=0 -> (1-x)/total : (n0'=0) & (n1'=0) & (n2'=0) & (n3'=0) & (n4'=0) & (n5'=0) & (n6'=0) & (n7'=0) & (h'=0) & (adv'=2) & (honest'=0) +
 			x/total : (n0'=min(w,n0+1)) & (adv'=0) & (honest'=0) +
 			n0*x/total : (n1'=min(w,n1+1)) & (adv'=0) & (honest'=0) +
 			n1*x/total : (n2'=min(w,n2+1)) & (h'=h+1-min(1,n2)) & (adv'=0) & (honest'=0) +
@@ -58,7 +59,8 @@ module SelfishMiningChia
 			n3*x/total : (n4'=min(w,n4+1)) & (h'=h+1-min(1,n4)) & (adv'=0) & (honest'=0) +
 			n4*x/total : (n5'=min(w,n5+1)) & (h'=h+1-min(1,n5)) & (adv'=0) & (honest'=0) +
 			n5*x/total : (n6'=min(w,n6+1)) & (h'=h+1-min(1,n6)) & (adv'=0) & (honest'=0) +
-			n6*x/total : (n0'=n1) & (n1'=n2) & (n2'=n3) & (n3'=n4) & (n4'=n5) & (n5'=n6) & (n6'=1) & (adv'=1) & (honest'=0); //TODO
+			n6*x/total : (n7'=min(w,n7+1)) & (h'=h+1-min(1,n7)) & (adv'=0) & (honest'=0) +
+			n7*x/total : (n0'=n1) & (n1'=n2) & (n2'=n3) & (n3'=n4) & (n4'=n5) & (n5'=n6) & (n6'=n7) & (n7'=1) & (adv'=1) & (honest'=0); //TODO
 			
 	
 	[] h>2 & c=0 -> (1-x)/total : (h'=h-1) & (adv'=1) & (honest'=0) +
@@ -69,11 +71,12 @@ module SelfishMiningChia
 			n3*x/total : (n4'=min(w,n4+1)) & (h'=h+1-min(1,n4)) & (adv'=0) & (honest'=0) +
 			n4*x/total : (n5'=min(w,n5+1)) & (h'=h+1-min(1,n5)) & (adv'=0) & (honest'=0) +
 			n5*x/total : (n6'=min(w,n6+1)) & (h'=h+1-min(1,n6)) & (adv'=0) & (honest'=0) +
-			n6*x/total : (n0'=n1) & (n1'=n2) & (n2'=n3) & (n3'=n4) & (n4'=n5) & (n5'=n6) & (n6'=1) & (adv'=1) & (honest'=0); //TODO
+			n6*x/total : (n7'=min(w,n7+1)) & (h'=h+1-min(1,n7)) & (adv'=0) & (honest'=0) +
+			n7*x/total : (n0'=n1) & (n1'=n2) & (n2'=n3) & (n3'=n4) & (n4'=n5) & (n5'=n6) & (n6'=n7) & (n7'=1) & (adv'=1) & (honest'=0); //TODO
 
 endmodule
 
 rewards
-    	[] true : honest;
+    	//[] true : honest;
     	[] true : adv;
 endrewards
