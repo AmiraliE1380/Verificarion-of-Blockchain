9508ad011e07167441134259379f0e8aefe8bb32
AmiraliE1380 <60638047+AmiraliE1380@users.noreply.github.com>
2023-05-31 11:43:08 +0330

    Add files via upload

--- MC model/MC_selfish_mining_chia_ver2.pm
+++ MC model/MC_selfish_mining_chia_ver2.pm
@@ -3,7 +3,7 @@ dtmc
 // x is fraction of space controlled by adv.
 const double x = 0.2;
 //y or gamma is the probability of honest mining on adv blocks during a race condition
-const double y = 0.5;
+const double y = 1;
 // d is the max lead the adv can have
 const int d = 6;
 // at each depth 6 private blocks can be saved.
@@ -37,42 +37,45 @@ module SelfishMiningChia
 	honest : [0..2] init 0;
 
 
-	[] h=0 -> x : (n1'=1) & (h'=1) & (adv'=0) & (honest'=0) +
+	[] h=0 -> x : (n0'=1) & (h'=1) & (adv'=0) & (honest'=0) +
 			(1-x) : (adv'=0) & (honest'=1); //DONE!
 
 
-	[] h=1 -> (n0+1)*x/total : (n1'=1) & (h'=2) & (adv'=0) & (honest'=0)  +
-			(1-x)/total : (c'=n0) & (n0'=0) & (h'=0) & (adv'=0) & (honest'=0) ; //DONE!
+	[] h=1 -> x/total : (n0'=min(w,n0+1)) & (adv'=0) & (honest'=0)  +
+			n0*x/total : (n1'=1) & (h'=2) & (adv'=0) & (honest'=0)  +
+			(1-x)/total : (c'=n0) & (n0'=0) & (h'=0) & (adv'=0) & (honest'=0) ; 
 	
 
-	[] c>0 -> (x*c)/(x*c+1-x) : (c'=0) & (adv=2) +
-			(1-x)*y/(x*c+1-x) : (c'=0) & (adv=1) & (honest=1) +
-			(1-x)*(1-y)/(x*c+1-x) : (c'=0) & (honest=2); //DONE!
+	[] c>0 -> (x*c)/(x*c+1-x) : (c'=0) & (adv'=2) +
+			(1-x)*y/(x*c+1-x) : (c'=0) & (adv'=1) & (honest'=1) +
+			(1-x)*(1-y)/(x*c+1-x) : (c'=0) & (honest'=2); 
 	
 
 	[] h=2 -> (1-x)/total : (n0'=0) & (n1'=0) & (n2'=0) & (n3'=0) & (n4'=0) & (n5'=0) & (n6'=0) & (h'=0) & (adv'=2) & (honest'=0) +
-			(n0+1)*x/total : (n1'=min(w,n1+1)) & (adv'=0) & (honest'=0) +
-			n1*x/total : (n2'=min(w,n2+1)) & (adv'=0) & (honest'=0) +
-			n2*x/total : (n3'=min(w,n3+1)) & (h'=max(h, 3)) & (adv'=0) & (honest'=0) +
-			n3*x/total : (n4'=min(w,n4+1)) & (h'=max(h, 4)) & (adv'=0) & (honest'=0) +
-			n4*x/total : (n5'=min(w,n5+1)) & (h'=max(h, 5)) & (adv'=0) & (honest'=0) +
-			n5*x/total : (n6'=min(w,n6+1)) & (h'=max(h, 6)) & (adv'=0) & (honest'=0) +
-			n6*x/total : (n0'=n1) & (n1'=n2) & (n2'=n3) & (n3'=n4) & (n4'=n5) & (n5'=n6) & (n6'=1) & (adv'=1) & (honest'=0); //DONE!
+			x/total : (n0'=min(w,n0+1)) & (adv'=0) & (honest'=0) +
+			n0*x/total : (n1'=min(w,n1+1)) & (adv'=0) & (honest'=0) +
+			n1*x/total : (n2'=min(w,n2+1)) & (h'=h+1-min(1,n2)) & (adv'=0) & (honest'=0) +
+			n2*x/total : (n3'=min(w,n3+1)) & (h'=h+1-min(1,n3)) & (adv'=0) & (honest'=0) +
+			n3*x/total : (n4'=min(w,n4+1)) & (h'=h+1-min(1,n4)) & (adv'=0) & (honest'=0) +
+			n4*x/total : (n5'=min(w,n5+1)) & (h'=h+1-min(1,n5)) & (adv'=0) & (honest'=0) +
+			n5*x/total : (n6'=min(w,n6+1)) & (h'=h+1-min(1,n6)) & (adv'=0) & (honest'=0) +
+			n6*x/total : (n0'=n1) & (n1'=n2) & (n2'=n3) & (n3'=n4) & (n4'=n5) & (n5'=n6) & (n6'=1) & (adv'=1) & (honest'=0); //TODO
 			
 	
 
 	[] h>2 -> (1-x)/total : (n0'=n1) & (n1'=n2) & (n2'=n3) & (n3'=n4) & (n4'=n5) & (n5'=n6) & (n6'=0) & (h'=h-1) & (adv'=1) & (honest'=0) +
-			(n0+1)*x/total : (n1'=min(w,n1+1)) & (adv'=0) & (honest'=0) +
+			x/total : (n0'=min(w,n0+1)) & (adv'=0) & (honest'=0) +
+			n0*x/total : (n1'=min(w,n1+1)) & (adv'=0) & (honest'=0) +
 			n1*x/total : (n2'=min(w,n2+1)) & (adv'=0) & (honest'=0) +
-			n2*x/total : (n3'=min(w,n3+1)) & (adv'=0) & (honest'=0) +
-			n3*x/total : (n4'=min(w,n4+1)) & (h'=max(h, 4)) & (adv'=0) & (honest'=0) +
-			n4*x/total : (n5'=min(w,n5+1)) & (h'=max(h, 5)) & (adv'=0) & (honest'=0) +
-			n5*x/total : (n6'=min(w,n6+1)) & (h'=max(h, 6)) & (adv'=0) & (honest'=0) +
-			n6*x/total : (n0'=n1) & (n1'=n2) & (n2'=n3) & (n3'=n4) & (n4'=n5) & (n5'=n6) & (n6'=1) & (adv'=1) & (honest'=0); //DONE!
+			n2*x/total : (n3'=min(w,n3+1)) & (h'=h+1-min(1,n3)) & (adv'=0) & (honest'=0) +
+			n3*x/total : (n4'=min(w,n4+1)) & (h'=h+1-min(1,n4)) & (adv'=0) & (honest'=0) +
+			n4*x/total : (n5'=min(w,n5+1)) & (h'=h+1-min(1,n5)) & (adv'=0) & (honest'=0) +
+			n5*x/total : (n6'=min(w,n6+1)) & (h'=h+1-min(1,n6)) & (adv'=0) & (honest'=0) +
+			n6*x/total : (n0'=n1) & (n1'=n2) & (n2'=n3) & (n3'=n4) & (n4'=n5) & (n5'=n6) & (n6'=1) & (adv'=1) & (honest'=0); //TODO
 
 endmodule
 
-rewards "adv"
-    	[] true : honest;
-    	//[] true : adv;
+rewards
+    	//[] true : honest;
+    	[] true : adv;
 endrewards
